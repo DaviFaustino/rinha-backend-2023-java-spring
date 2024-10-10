15b9b999b87b1616bea5e765054e2ed3871c7e91
DaviFaustino <davifaustino05@gmail.com>
2024-10-10 17:02:03 -0300

    feat: create PessoaRepository

---
+++ src/main/java/com/davifaustino/rinhabackend/infrastructure/PessoaRepository.java
@@ -0,0 +1,28 @@
+package com.davifaustino.rinhabackend.infrastructure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.davifaustino.rinhabackend.domain.Pessoa;
+import com.davifaustino.rinhabackend.domain.PessoaRepositoryPort;
+
+import java.util.UUID;
+
+@Component
+public class PessoaRepository implements PessoaRepositoryPort {
+
+    @Autowired
+    private SpringPessoaRepository springPessoaRepository;
+
+    @Override
+    public boolean existsByApelido(String apelido) {
+        return springPessoaRepository.existsByApelido(apelido);
+    }
+
+    @Override
+    public UUID savePessoa(Pessoa pessoa) {
+        springPessoaRepository.save(new PessoaEntity(pessoa));
+        
+        return pessoa.getId();
+    }
+}
