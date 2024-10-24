2a6339c081468c7631d07b94c7f9edbed2e7f4fd
DaviFaustino <davifaustino.dev@gmail.com>
2024-10-24 16:30:10 -0300

    feat: create global exception handler

---
+++ src/main/java/com/davifaustino/rinhabackend/application/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.davifaustino.rinhabackend.application;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.davifaustino.rinhabackend.domain.BadRequestException;
+import com.davifaustino.rinhabackend.domain.UnprocessableException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), request.getRequestURI(), request.getMethod());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(UnprocessableException.class)
+    public ResponseEntity<ErrorResponse> handleUnprocessableException(UnprocessableException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), request.getRequestURI(), request.getMethod());
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse("Http message not readable", request.getRequestURI(), request.getMethod());
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+}
