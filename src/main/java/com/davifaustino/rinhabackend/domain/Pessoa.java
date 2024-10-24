cfbe72e4646c194c930ebd42be08f87cbe638d85
DaviFaustino <davifaustino.dev@gmail.com>
2024-10-24 16:33:29 -0300

    fix: fix Pessoa validation

--- src/main/java/com/davifaustino/rinhabackend/domain/Pessoa.java
+++ src/main/java/com/davifaustino/rinhabackend/domain/Pessoa.java
@@ -1,5 +1,7 @@
 package com.davifaustino.rinhabackend.domain;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,19 +22,31 @@ public class Pessoa {
     }
 
     public Pessoa(PessoaDto pessoaDto) {
-        
-        if (pessoaDto.apelido() == null || pessoaDto.nome() == null || pessoaDto.nascimento() == null) {
-            throw new UnprocessableException("Valor nulo");
-        }
-        if (pessoaDto.stack() != null && pessoaDto.stack().isEmpty()) {
-            throw new BadRequestException("Stack vazia");
-        }
+        validar(pessoaDto);
 
         this.apelido = pessoaDto.apelido();
         this.nome = pessoaDto.nome();
         this.nascimento = pessoaDto.nascimento();
         this.stack = pessoaDto.stack();
         this.id = UUID.randomUUID();
+    }
+
+    private void validar(PessoaDto pessoaDto) {
+
+        if (pessoaDto.apelido() == null || pessoaDto.nome() == null || pessoaDto.nascimento() == null) {
+            throw new UnprocessableException("Valor de campo nulo");
+        }
+        if (pessoaDto.apelido().matches("[0-9.]+") || pessoaDto.nome().matches("[0-9.]+") || pessoaDto.nascimento().matches("[0-9.]+")) {
+            throw new BadRequestException("Valor numerico em vez de string");
+        }
+        try {
+            LocalDate.parse(pessoaDto.nascimento(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } catch (Exception e) {
+            throw new UnprocessableException("Nascimento em formato errado");
+        }
+        if (pessoaDto.stack() != null && pessoaDto.stack().isEmpty()) {
+            throw new BadRequestException("Stack vazia");
+        }
     }
 
     public UUID getId() {
