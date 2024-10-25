e2feebec75b026a7fe7f9a9c74c49a602687d6d5
DaviFaustino <davifaustino.dev@gmail.com>
2024-10-25 12:13:49 -0300

    feat: add test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_pessoas
+VALUES ('3f306ba4-bcb1-4c7c-bfcb-78a747225eda', 'Mario', 'Jorge', '1980-07-08', ARRAY['Java']),
+('883e2924-55d1-4b03-aa2f-a29c2dfe9937', 'Dino', 'Ramom', '1995-06-07', ARRAY['Ruby', 'Rails']),
+('5a62be45-0fc1-40fc-9e71-01dd08183dae', 'Chiquinha', 'Francisca', '1990-05-06', ARRAY['Javascript', 'Vue.js']),
+('4d05f65f-ce37-4df7-8215-e5439c9b0303', 'Bola', 'Mateus', '1988-04-05', ARRAY['C#', 'Docker']),
+('8f25aac2-a676-4561-b712-f0545d3f0803', 'Lana', 'Alana', '2002-04-05', ARRAY['Java', 'Spring framework', 'PostgreSQL']);
