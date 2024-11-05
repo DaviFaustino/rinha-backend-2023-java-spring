1035b6770dff06c4ed90a9cd9c17a981f5081a0d
DaviFaustino <davifaustino.dev@gmail.com>
2024-11-05 19:45:44 -0300

    feat: create method in the Spring repository to get pessoas

--- src/main/java/com/davifaustino/rinhabackend/infrastructure/SpringPessoaRepository.java
+++ src/main/java/com/davifaustino/rinhabackend/infrastructure/SpringPessoaRepository.java
@@ -1,12 +1,20 @@
 package com.davifaustino.rinhabackend.infrastructure;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SpringPessoaRepository extends JpaRepository<PessoaEntity, UUID> {
     
     boolean existsByApelido(String apelido);
+
+    @Query(value = "SELECT * FROM tb_pessoas " +
+                    "WHERE busca  ILIKE CONCAT('%', :termo, '%') " +
+                    "LIMIT 50;", nativeQuery = true)
+    List<PessoaEntity> findByBuscaIgnoreCaseContaining(@Param("termo") String termo);
 }
